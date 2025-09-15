939c28ab5b2cc24082cb06b6a7621c4af3df0480
Andy Reading <andy@castlegateit.co.uk>
2025-09-15 10:21:33 +0100

    Added support for legacy WooCommerce REST API endpoints to bypass the protection.

--- cgit-wp-password-protection.php
+++ cgit-wp-password-protection.php
@@ -4,7 +4,7 @@
  * Plugin Name:  Castlegate IT WP Password Protection
  * Plugin URI:   https://github.com/castlegateit/cgit-wp-password-protection
  * Description:  Password protect a WordPress site.
- * Version:      1.2.0
+ * Version:      1.2.1
  * Requires PHP: 8.2
  * Author:       Castlegate IT
  * Author URI:   https://www.castlegateit.co.uk/
@@ -16,7 +16,7 @@ if (!defined('ABSPATH')) {
     wp_die('Access denied');
 }
 
-define('CGIT_WP_PASSWORD_PROTECTION_VERSION', '1.2.0');
+define('CGIT_WP_PASSWORD_PROTECTION_VERSION', '1.2.1');
 define('CGIT_WP_PASSWORD_PROTECTION_PLUGIN_FILE', __FILE__);
 define('CGIT_WP_PASSWORD_PROTECTION_PLUGIN_DIR', __DIR__);
 
@@ -183,6 +183,13 @@ function is_rest_api_request(): bool {
     // Must start with /wp-json or exactly /wp-json
     if (str_starts_with($request_path, '/' . $prefix . '/') || $request_path === '/'.$prefix) {
         return true;
+    }
+
+    // Check old WooCommerce endpoint. Must start with but not be equal to /wc-api/
+    if (class_exists('WooCommerce')) {
+        if (str_starts_with($request_path, '/wc-api/') && $request_path !== '/wc-api/') {
+            return true;
+        }
     }
 
     // Fallback: ?rest_route=/ style requests (plain permalinks)
